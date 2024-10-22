f8fa96197dc3e4f1c38a1e495f0982c3fabb74b1
elivandro <elivandro78@gmail.com>
2024-10-22 00:22:57 -0300

    corrigindo url

--- src/app/autenticacao/auth/conectionActive.ts
+++ src/app/autenticacao/auth/conectionActive.ts
@@ -9,7 +9,6 @@ import { Conect } from './Conect.service';
     providedIn:"root"
 })
 export class ConexaoActive implements CanActivate{
-  url = "https://agileservice.onrender.com";
     constructor(
         private router: Router,
         private dialog: MatDialog,
@@ -21,7 +20,7 @@ export class ConexaoActive implements CanActivate{
           this.dialog.open(InternetErroComponent);
           return false;
         } else {
-          return this.connectivityService.checkConnectivity("http://localhost:4200/", 20000).pipe(
+          return this.connectivityService.checkConnectivity("https://agileservice.onrender.com", 10000).pipe(
             map((isConnected: boolean) => {
               if (!isConnected) {
                 this.dialog.open(InternetErroComponent);
